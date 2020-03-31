57e351d1fee1e0a334b9d1cb5b82725794636f38
Ahmed Ali <ahmed.ali@ibtikar.net.sa>
2020-03-31 19:17:49 +0200

    Make shell interactive with user

--- generate_laravel_project.sh
+++ generate_laravel_project.sh
@@ -1,13 +1,48 @@
 #!/usr/bin/env bash
 
-if [ $# -eq 0 ]
-  then
-    echo "No arguments supplied, Please provide project name"
-fi
+getParameters() {
+    printf "Path to the folder \n"
 
-root=/var/dev/$1
+    read path
 
-resources_dir=/var/dev/generate_laravel_project
+    printf "\nGive me the folder name \n"
+
+    read folder_name
+}
+
+# Check if path exist
+checkPathExist() {
+    echo "$1"
+    if [ ! -d "$1" ]
+    then
+        printf "Directory %s doesn't exist. \n" "$1"
+        exit
+    fi
+}
+
+# Add end backslash
+addEndBackSlash()
+{
+    STR="$1"
+
+    length=${#STR}
+    last_char=${STR:length-1:1}
+
+    [[ $last_char != "/" ]] && STR="$STR/"; :
+
+    echo $STR
+}
+
+printf "Welcome \n"
+
+getParameters
+checkPathExist $path
+
+path=$(addEndBackSlash $path)
+
+root=$path$folder_name
+
+resources_dir=/var/dev/Projects/generate_laravel_project
 
 # making project root
 mkdir $root
@@ -22,8 +57,10 @@ rm -rf laravel.zip
 
 echo $root/app
 
-docker run -it --rm -u 1000 -v $root/app:/app -w /app composer:1.8.6 /bin/bash -c "composer self-update && composer global require hirak/prestissimo && composer install"
+docker run -it --rm -v $root/app:/app -w /app composer:1.8.6 /bin/bash -c "composer self-update && composer global require hirak/prestissimo && composer install"
 
 sudo chown -R ahmed:ahmed $root/app/vendor
 
 cp $root/app/.env.example $root/app/.env
+
+chown -R 1000:1000 $root/app/storage
